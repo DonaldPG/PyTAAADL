89c7011a09f1a93b5225b762bed7e458a7de44a5
Donald Griffith <donald.griffith@shell.com>
2018-01-17 17:39:55 -0600

    added squeeze-excite blocks

--- PyTAAADL__0_train_many_DL_models.py
+++ PyTAAADL__0_train_many_DL_models.py
@@ -9,6 +9,8 @@ import datetime
 import numpy as np
 
 from keras import backend as K
+from keras.models import Input
+from keras.models import Model
 from keras.models import Sequential
 from keras.models import model_from_json
 from keras.layers import Conv2D
@@ -53,7 +55,129 @@ from functions.TAfunctions import _is_odd, \
                                   cleantoend
 from functions.UpdateSymbols_inHDF5 import UpdateHDF5, \
                                            loadQuotes_fromHDF
+from functions.se import squeeze_excite_block
 os.chdir(_cwd)
+
+# --------------------------------------------------
+# Build standard keras model
+# --------------------------------------------------
+def build_model(Xtrain, number_feature_maps, perform_batch_normalization,
+                   use_leaky_relu, use_separable ):
+
+    model = Sequential()
+
+    for i, nfeature in enumerate(number_feature_maps):
+        if i == 0:
+            model.add(Conv2D(nfeature, kernel_size=(3, 1), padding='same',
+                             strides=(1, 1), data_format='channels_last',
+                             kernel_initializer='lecun_uniform', bias_initializer='zeros',
+                             input_shape=(Xtrain.shape[1], Xtrain.shape[2], 3)))
+        else:
+            if perform_batch_normalization is True:
+                model.add(Conv2D(nfeature, kernel_size=(3, 1), padding='same',
+                             strides=(1, 1), data_format='channels_last',
+                             use_bias=False))
+                model.add(BatchNormalization())
+            else:
+                model.add(Conv2D(nfeature, kernel_size=(3, 1), padding='same',
+                             strides=(1, 1), data_format='channels_last',
+                             use_bias=True))
+        #model.add(GaussianNoise(.0001))
+        if use_leaky_relu is False:
+            model.add(Activation('relu'))
+        else:
+            model.add(LeakyReLU(alpha=leaky_relu_alpha))
+        if i > 2:
+            model.add(Dropout(dropout_pct))
+        #if _is_odd(i) or i == number_feature_maps[-1]:
+        #    model.add(MaxPooling2D(pool_size=(2, 1)))
+        print("i, model shape = ", i, model.output_shape)
+
+    nfeature = 16
+    for i in range(2):
+        if use_separable == False:
+            model.add(Conv2D(nfeature, kernel_size=(1, 3), padding='same', strides=(1, 1),
+                             data_format='channels_last', use_bias=False))
+        else:
+            model.add(Dense(16))
+            print("i, before separableConv2D, model shape = ", i, model.output_shape)
+            model.add(SeparableConv2D(nfeature, kernel_size=(1, 3), padding='same',
+                                      strides=(1, 1), data_format='channels_last',
+                                      depth_multiplier=1, use_bias=False))
+        model.add(BatchNormalization())
+        model.add(Activation('relu'))
+
+    model.add(Flatten())
+    model.add(Dense(int(64./dense_factor), kernel_initializer='lecun_uniform', bias_initializer='zeros'))
+    model.add(Dense(int(32./dense_factor)))
+    model.add(Dense(int(16./dense_factor)))
+    model.add(Dense(1))
+    print("final model shape = ", model.output_shape)
+
+    return model
+
+# --------------------------------------------------
+# Build keras model with squeeze-excite block
+# --------------------------------------------------
+
+def build_se_model(Xtrain, number_feature_maps, perform_batch_normalization,
+                   use_leaky_relu, use_separable ):
+    #model = Sequential()
+    K.clear_session()
+    inputs = Input(shape=((Xtrain.shape[1], Xtrain.shape[2], 3)))
+
+    for i, nfeature in enumerate(number_feature_maps):
+        if i == 0:
+            x = Conv2D(nfeature, kernel_size=(3, 1), padding='same',
+                             strides=(1, 1), data_format='channels_last',
+                             kernel_initializer='lecun_uniform', bias_initializer='zeros')(inputs)
+        else:
+            if perform_batch_normalization is True:
+                x = Conv2D(nfeature, kernel_size=(3, 1), padding='same',
+                             strides=(1, 1), data_format='channels_last',
+                             use_bias=False)(x)
+                x = BatchNormalization()(x)
+            else:
+                x = Conv2D(nfeature, kernel_size=(3, 1), padding='same',
+                             strides=(1, 1), data_format='channels_last',
+                             use_bias=True)(x)
+        #model.add(GaussianNoise(.0001))
+        if use_leaky_relu is False:
+            x = Activation('relu')(x)
+        else:
+            x = LeakyReLU(alpha=leaky_relu_alpha)(x)
+        if i > 2:
+            x = Dropout(dropout_pct)(x)
+        #if _is_odd(i) or i == number_feature_maps[-1]:
+        #    model.add(MaxPooling2D(pool_size=(2, 1)))
+        print("i, model shape = ", i, K.shape(x))
+
+        x = squeeze_excite_block(x, ratio=16)
+
+    nfeature = 16
+    for i in range(2):
+        if use_separable == False:
+            x = Conv2D(nfeature, kernel_size=(1, 3), padding='same', strides=(1, 1),
+                             data_format='channels_last', use_bias=False)(x)
+        else:
+            x = Dense(16)(x)
+            print("i, before separableConv2D, model shape = ", i, K.shape(x))
+            x = SeparableConv2D(nfeature, kernel_size=(1, 3), padding='same',
+                                      strides=(1, 1), data_format='channels_last',
+                                      depth_multiplier=1, use_bias=False)(x)
+        x = BatchNormalization()(x)
+        x = Activation('relu')(x)
+
+    x = Flatten()(x)
+    x = Dense(int(64./dense_factor), kernel_initializer='lecun_uniform', bias_initializer='zeros')(x)
+    x = Dense(int(32./dense_factor))(x)
+    x = Dense(int(16./dense_factor))(x)
+    predictions = Dense(1)(x)
+    model = Model(inputs=inputs, outputs=predictions)
+    print("final model shape = ", model.output_shape)
+
+    return model
+
 
 
 # --------------------------------------------------
@@ -170,7 +294,7 @@ try:
 except:
     pass
 
-for itrial in range(4,25):
+for itrial in range(25):
 
     start_time = time.time()
 
@@ -453,59 +577,17 @@ for itrial in range(4,25):
     # --------------------------------------------------
     # build DL model
     # --------------------------------------------------
-
-    model = Sequential()
-
-    for i, nfeature in enumerate(number_feature_maps):
-        if i == 0:
-            model.add(Conv2D(nfeature, kernel_size=(3, 1), padding='same',
-                             strides=(1, 1), data_format='channels_last',
-                             kernel_initializer='lecun_uniform', bias_initializer='zeros',
-                             input_shape=(Xtrain.shape[1], Xtrain.shape[2], 3)))
-        else:
-            if perform_batch_normalization is True:
-                model.add(Conv2D(nfeature, kernel_size=(3, 1), padding='same',
-                             strides=(1, 1), data_format='channels_last',
-                             use_bias=False))
-                model.add(BatchNormalization())
-            else:
-                model.add(Conv2D(nfeature, kernel_size=(3, 1), padding='same',
-                             strides=(1, 1), data_format='channels_last',
-                             use_bias=True))
-        #model.add(GaussianNoise(.0001))
-        if use_leaky_relu is False:
-            model.add(Activation('relu'))
-        else:
-            model.add(LeakyReLU(alpha=leaky_relu_alpha))
-        if i > 2:
-            model.add(Dropout(dropout_pct))
-        #if _is_odd(i) or i == number_feature_maps[-1]:
-        #    model.add(MaxPooling2D(pool_size=(2, 1)))
-        print("i, model shape = ", i, model.output_shape)
-
-    nfeature = 16
-    for i in range(2):
-        if use_separable == False:
-            model.add(Conv2D(nfeature, kernel_size=(1, 3), padding='same', strides=(1, 1),
-                             data_format='channels_last', use_bias=False))
-        else:
-            model.add(Dense(16))
-            print("i, before separableConv2D, model shape = ", i, model.output_shape)
-            model.add(SeparableConv2D(nfeature, kernel_size=(1, 3), padding='same',
-                                      strides=(1, 1), data_format='channels_last',
-                                      depth_multiplier=1, use_bias=False))
-        model.add(BatchNormalization())
-        model.add(Activation('relu'))
-
-    model.add(Flatten())
-    model.add(Dense(int(64./dense_factor), kernel_initializer='lecun_uniform', bias_initializer='zeros'))
-    model.add(Dense(int(32./dense_factor)))
-    model.add(Dense(int(16./dense_factor)))
-    model.add(Dense(1))
-    print("final model shape = ", model.output_shape)
+    use_se_block = np.random.choice([True, True, True, True, False])
+    if use_se_block is True:
+        model = build_se_model(Xtrain, number_feature_maps, perform_batch_normalization,
+                   use_leaky_relu, use_separable )
+    else:
+        model = build_model(Xtrain, number_feature_maps, perform_batch_normalization,
+                   use_leaky_relu, use_separable )
 
     model.summary()
     print("feature_map_factor = ", feature_map_factor)
+    print("use_se_block = ", use_se_block)
 
     if optimizer_choice == 'RMSprop':
         optimizer = RMSprop(lr=learning_rate, rho=0.9, epsilon=1e-08, decay=0.0)
@@ -543,9 +625,10 @@ for itrial in range(4,25):
         f.write("feature_map_factor = "+str(feature_map_factor)+"\n")
         f.write("perform_batch_normalization: "+str(perform_batch_normalization)+"\n")
         f.write("dense_factor: "+str(dense_factor)+"\n")
-        f.write("use_leaky_relu: "+str(use_leaky_relu)+"\n")
         f.write("use_separable: "+str(use_separable)+"\n")
+        f.write("use_leaky_relu: "+str(use_leaky_relu)+"\n")
         f.write("leaky_relu_alpha: "+str(leaky_relu_alpha)+"\n")
+        f.write("use_se_block: "+str(use_se_block)+"\n")
         f.write("optimizer_choice: "+optimizer_choice+"\n")
         f.write("learning_rate: "+str(learning_rate)+"\n")
         f.write("loss function: "+loss_function+"\n")
